213a241fb65c638f2fbd3bd3cf32528fd76b5287
MAXIM-SYSOEV\WorkPC <work@maks4a.gmail>
2020-07-13 16:35:13 +0300

    Тест Should_Send_Contact

---
+++ Tests/Integ/Sending Messages/SendingContactMessageTests.pas
@@ -0,0 +1,51 @@
+﻿unit SendingContactMessageTests;
+
+interface
+
+uses
+  TestsFixture,
+  TelegramBotApi.Client,
+  Test.Data,
+  DUnitX.TestFramework,
+  DUnitX.Attributes;
+
+type
+
+  [TestFixture]
+  TSendingContactMessageTests = class(TTestsFixture)
+  private
+    FLastID: string;
+  public
+    [Test]
+    procedure Should_Send_Contact;
+  end;
+
+implementation
+
+uses
+  TelegramBotApi.Types.Request,
+  TelegramBotApi.Types,
+  TelegramBotApi.Types.Enums;
+
+{ TSendingContactMessageTests }
+
+procedure TSendingContactMessageTests.Should_Send_Contact;
+var
+  LSendContactArgument: TtgSendContactArgument;
+  LResult: ItgResponse<TtgMessage>;
+
+begin
+  LSendContactArgument := TtgSendContactArgument.Default;
+  LSendContactArgument.ChatId := TTestData.Current.SupergroupChat.ID;
+  LSendContactArgument.PhoneNumber := '+38 063 126 48 33';
+  LSendContactArgument.FirstName := 'Max';
+  LSendContactArgument.LastName := 'Rare';
+  LResult := Bot.SendContact(LSendContactArgument);
+
+  Assert.AreEqual(True, LResult.Ok, LResult.Description);
+  Assert.AreEqual(LSendContactArgument.PhoneNumber, LResult.Result.Contact.PhoneNumber);
+  Assert.AreEqual(LSendContactArgument.FirstName, LResult.Result.Contact.FirstName);
+  Assert.AreEqual(LSendContactArgument.LastName, LResult.Result.Contact.LastName);
+end;
+
+end.
