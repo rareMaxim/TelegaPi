d7ae4e194e9566d3dbbeb0c60f179f93cddcc8a0
maks4a <maks4a@gmail.com>
2020-08-17 21:23:07 +0300

    Улучшил тест Should_Send_Poll

--- Tests/Integ/Pools/AnonymousPollTests.pas
+++ Tests/Integ/Pools/AnonymousPollTests.pas
@@ -21,8 +21,10 @@ type
 implementation
 
 uses
+  TelegramBotApi.Types,
+  TelegramBotApi.Types.Enums,
   TelegramBotApi.Types.Request,
-  TelegramBotApi.Types, TelegramBotApi.Types.Enums;
+  Test.Assert;
 
 { TAnonymousPollTests }
 
@@ -47,7 +49,13 @@ begin
   Assert.IsTrue(LMessage.Poll.IsAnonymous);
   Assert.AreEqual('regular', LMessage.Poll.&Type);
   Assert.IsFalse(LMessage.Poll.AllowsMultipleAnswers);
-
+  TgAssert.IsDefault(LMessage.Poll.CorrectOptionId);
+  TgAssert.IsDefault(LMessage.Poll.OpenPeriod);
+  TgAssert.IsDefault(LMessage.Poll.CloseDate);
+  Assert.AreEqual(LPool.Question, LMessage.Poll.Question);
+  Assert.AreEqual(3, Length(LMessage.Poll.Options));
+  for i := Low(LPool.Options) to High(LPool.Options) do
+    Assert.AreEqual(LPool.Options[i], LMessage.Poll.Options[i].Text);
 end;
 
 end.
