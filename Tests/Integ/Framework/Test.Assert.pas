cd4ffd2ecd311f5f40cf8675866eb0a8eb2b6242
maks4a <maks4a@gmail.com>
2020-08-17 21:21:00 +0300

    Расширен TgAssert

--- Tests/Integ/Framework/Test.Assert.pas
+++ Tests/Integ/Framework/Test.Assert.pas
@@ -9,6 +9,8 @@ type
   TgAssert = class(Assert)
     class procedure InRange(actual, low, hight: single; const message: string = ''); overload;
     class procedure InRange(actual, low, hight: int64; const message: string = ''); overload;
+    class procedure IsDefault<T>(actual: T; const message: string = ''); overload;
+    class procedure IsDefault(actual: integer; const message: string = ''); overload;
   end;
 
 implementation
@@ -34,6 +36,16 @@ begin
     FailFmt('%f < %f. %s', [actual, low, message], ReturnAddress)
   else if System.Math.CompareValue(actual, hight) = 1 then
     FailFmt('%f > %f. %s', [actual, hight, message], ReturnAddress);
+end;
+
+class procedure TgAssert.IsDefault(actual: integer; const message: string);
+begin
+  IsDefault<integer>(actual);
+end;
+
+class procedure TgAssert.IsDefault<T>(actual: T; const message: string);
+begin
+  AreEqual(Default (T), actual);
 end;
 
 end.
