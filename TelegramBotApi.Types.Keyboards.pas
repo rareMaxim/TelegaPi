20af7d7ab78a9f647d73f06dc66d2c53d979b5ce
MAXIM-SYSOEV\WorkPC <work@rare.max>
2020-11-19 11:48:19 +0200

    Добавлен тип TtgReplyKeyboardRemove

--- TelegramBotApi.Types.Keyboards.pas
+++ TelegramBotApi.Types.Keyboards.pas
@@ -119,6 +119,39 @@ type
     { TODO -oOwner -cGeneral : Заполнить }
   end;
 
+  /// <summary>
+  /// Upon receiving a message with this object, Telegram clients will remove the
+  /// current custom keyboard and display the default letter-keyboard. By default,
+  /// custom keyboards are displayed until a new keyboard is sent by a bot. An
+  /// exception is made for one-time keyboards that are hidden immediately after the
+  /// user presses a button (see ReplyKeyboardMarkup).
+  /// </summary>
+  TtgReplyKeyboardRemove = class
+  private
+    [JsonName('remove_keyboard')]
+    FRemoveKeyboard: Boolean;
+    [JsonName('selective')]
+    FSelective: Boolean;
+  public
+    /// <summary>
+    /// Requests clients to remove the custom keyboard (user will not be able to summon
+    /// this keyboard; if you want to hide the keyboard from sight but keep it
+    /// accessible, use one_time_keyboard in ReplyKeyboardMarkup)
+    /// </summary>
+    property RemoveKeyboard: Boolean read FRemoveKeyboard write FRemoveKeyboard;
+    /// <summary>
+    /// Optional. Use this parameter if you want to remove the keyboard for specific
+    /// users only. Targets: 1) users that are @mentioned in the text of the Message
+    /// object; 2) if the bot's message is a reply (has reply_to_message_id), sender of
+    /// the original message.
+    ///
+    /// Example: A user votes in a poll, bot returns confirmation message in reply to
+    /// the vote and removes the keyboard for that user, while still showing the
+    /// keyboard with poll options to users who haven't voted yet.
+    /// </summary>
+    property Selective: Boolean read FSelective write FSelective;
+  end;
+
 implementation
 
 end.
