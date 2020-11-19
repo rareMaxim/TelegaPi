a02e0e4e04bfa5253b6f625c11e9d184bcfbd6e0
MAXIM-SYSOEV\WorkPC <work@rare.max>
2020-11-19 11:45:08 +0200

    Добавлен тип TtgKeyboardButtonPollType. Обновлен тип TtgKeyboardButton.

--- TelegramBotApi.Types.Keyboards.pas
+++ TelegramBotApi.Types.Keyboards.pas
@@ -6,6 +6,36 @@ uses
   System.JSON.Serializers;
 
 type
+
+  /// <summary>
+  /// This object represents type of a poll, which is allowed to be created and sent
+  /// when the corresponding button is pressed.
+  /// </summary>
+  TtgKeyboardButtonPollType = class
+  private
+    FType: string;
+  public
+    /// <summary>
+    /// Optional. If quiz is passed, the user will be allowed to create only polls in
+    /// the quiz mode. If regular is passed, only regular polls will be allowed.
+    /// Otherwise, the user will be allowed to create a poll of any type.
+    /// </summary>
+    { TODO -oOwner -cGeneral : change to enum }
+    property &Type: string read FType write FType;
+  end;
+
+  /// <summary> This object represents one button of the reply keyboard. For simple
+  /// text buttons String can be used instead of this object to specify text of the
+  /// button. Optional fields request_contact, request_location, and request_poll are
+  /// mutually exclusive
+  /// </summary>
+  /// <remarks>
+  /// Note: request_contact and request_location options will only work in Telegram
+  /// versions released after 9 April, 2016. Older clients will display unsupported
+  /// message.
+  /// Note: request_poll option will only work in Telegram versions released after 23
+  /// January, 2020. Older clients will display unsupported message.
+  /// </remarks>
   TtgKeyboardButton = class
   private
     [JsonName('can_join_groups')]
@@ -14,10 +44,29 @@ type
     FRequestContact: Boolean;
     [JsonName('request_location')]
     FRequestLocation: Boolean;
+    [JsonName('request_poll')]
+    FRequestPoll: TtgKeyboardButtonPollType;
   public
+    /// <summary>
+    /// Text of the button. If none of the optional fields are used, it will be sent as
+    /// a message when the button is pressed
+    /// </summary>
     property Text: string read FText write FText;
+    /// <summary>
+    /// Optional. If True, the user's phone number will be sent as a contact when the
+    /// button is pressed. Available in private chats only
+    /// </summary>
     property RequestContact: Boolean read FRequestContact write FRequestContact;
+    /// <summary>
+    /// Optional. If True, the user's current location will be sent when the button is
+    /// pressed. Available in private chats only
+    /// </summary>
     property RequestLocation: Boolean read FRequestLocation write FRequestLocation;
+    /// <summary>
+    /// Optional. If specified, the user will be asked to create a poll and send it to
+    /// the bot when the button is pressed. Available in private chats only
+    /// </summary>
+    property RequestPoll: TtgKeyboardButtonPollType read FRequestPoll write FRequestPoll;
   end;
 
   /// <summary>
