e823a42e72cfe4688381a9ff0f140c4a61918c3f
maks4a <maks4a@gmail.com>
2020-11-20 22:46:58 +0200

    Добавлен тип TtgForceReply

--- TelegramBotApi.Types.Keyboards.pas
+++ TelegramBotApi.Types.Keyboards.pas
@@ -300,6 +300,33 @@ type
   end;
 {$ENDREGION}
 
+  /// <summary>
+  /// Upon receiving a message with this object, Telegram clients will display a
+  /// reply interface to the user (act as if the user has selected the bot's message
+  /// and tapped 'Reply'). This can be extremely useful if you want to create
+  /// user-friendly step-by-step interfaces without having to sacrifice privacy mode.
+  /// </summary>
+  TtgForceReply = class
+  private
+    [JsonName('force_reply')]
+    FForceReply: Boolean;
+    [JsonName('selective')]
+    FSelective: Boolean;
+  public
+    /// <summary>
+    /// Shows reply interface to the user, as if they manually selected the bot's
+    /// message and tapped 'Reply'
+    /// </summary>
+    property ForceReply: Boolean read FForceReply write FForceReply;
+    /// <summary>
+    /// Optional. Use this parameter if you want to force reply from specific users
+    /// only. Targets: 1) users that are @mentioned in the text of the Message object;
+    /// 2) if the bot's message is a reply (has reply_to_message_id), sender of the
+    /// original message.
+    /// </summary>
+    property Selective: Boolean read FSelective write FSelective;
+  end;
+
 implementation
 
 end.
