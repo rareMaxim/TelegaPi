f5abde70a88218b385750498660375dbd02e2dd4
Maxim Sysoev <maks4a@gmail.com>
2021-11-06 20:01:48 +0200

    [ WIP ] Telegram Router - модуль для упрощения работы с маршрутами бизнес-логики

---
+++ Demos/Router/Router.dpr
@@ -0,0 +1,180 @@
+﻿program Router;
+
+{$APPTYPE CONSOLE}
+{$R *.res}
+
+uses
+  TelegramBotApi.Client,
+  TelegramBotApi.Types,
+  TelegramBotApi.Types.Enums,
+  TelegramBotApi.Types.Request,
+  TelegramBotApi.Polling.Console,
+  System.SysUtils,
+  System.Rtti,
+  Winapi.Windows, TelegramBotApi.Types.Keyboards;
+
+type
+  TDemoPooling = class
+  const
+    BOT_TOKEN = '1225990942:AAEfSINTq5fMdAOiswxNScZ8wQUDD_5KDYQ';
+  private
+    fBot: TTelegramBotApi;
+    fPooling: TtgPollingConsole;
+  protected
+    procedure UpdateConsoleTitle(ABot: TTelegramBotApi);
+    procedure SendTextMessage(const UserLink: TtgUserLink; const MsgText: string);
+    procedure DoReadMessage(AMsg: TtgMessage);
+    procedure SendFile(AMsg: TtgMessage);
+    procedure SendReplyKeyboard(AMsg: TtgMessage);
+  public
+    procedure Main;
+    constructor Create;
+    destructor Destroy; override;
+
+  end;
+
+  { TEchoCore }
+
+constructor TDemoPooling.Create;
+begin
+  fBot := TTelegramBotApi.Create(BOT_TOKEN);
+  fPooling := TtgPollingConsole.Create;
+  fPooling.Bot := fBot;
+end;
+
+destructor TDemoPooling.Destroy;
+begin
+  fBot.Free;
+  fPooling.Free;
+  inherited;
+end;
+
+procedure TDemoPooling.DoReadMessage(AMsg: TtgMessage);
+var
+  lMsgType: string;
+  lAction: string;
+begin
+  lMsgType := TRttiEnumerationType.GetName<TtgMessageType>(AMsg.&Type);
+  Writeln('Receive message type: ' + lMsgType);
+  if AMsg.&Type = TtgMessageType.Text then
+  begin
+    lAction := AMsg.Text.Split([' '])[0];
+    if lAction = '/photo' then
+    begin
+      SendFile(AMsg);
+    end
+    else if lAction = '/keyboard' then
+    begin
+      SendReplyKeyboard(AMsg);
+    end
+    else
+      SendTextMessage(AMsg.Chat.ID, AMsg.Text);
+  end
+  else
+    SendTextMessage(AMsg.Chat.ID, lMsgType + ': ' + AMsg.Text);
+end;
+
+procedure TDemoPooling.Main;
+begin
+  UpdateConsoleTitle(fBot);
+  fPooling.OnMessage := procedure(AMsg: TtgMessage)
+    begin
+      DoReadMessage(AMsg);
+    end;
+  fPooling.Start;
+end;
+
+procedure TDemoPooling.SendFile(AMsg: TtgMessage);
+var
+  lChatActionArg: TtgSendChatActionArgument;
+  lSendPhotoArg: TtgSendPhotoArgument;
+begin
+  lChatActionArg := TtgSendChatActionArgument.Create(AMsg.Chat.ID, TtgChatAction.Typing);
+  try
+    fBot.SendChatAction(lChatActionArg);
+  finally
+    lChatActionArg.Free;
+  end;
+
+  lSendPhotoArg := TtgSendPhotoArgument.Create;
+  try
+    lSendPhotoArg.Photo := 'https://telegram.org/img/t_logo.png?1';
+    lSendPhotoArg.Caption := 'Nice Picture';
+    lSendPhotoArg.ChatId := AMsg.Chat.ID;
+    fBot.SendPhoto(lSendPhotoArg);
+  finally
+    lSendPhotoArg.Free;
+  end;
+end;
+
+procedure TDemoPooling.SendReplyKeyboard(AMsg: TtgMessage);
+var
+  lKB: TtgReplyKeyboardMarkup;
+  lBtn: TtgKeyboardButton;
+  lMsg: TtgMessageArgument;
+begin
+  lMsg := TtgMessageArgument.Create;
+  lKB := TtgReplyKeyboardMarkup.Create;
+  lBtn := TtgKeyboardButton.Create;
+  try
+    lBtn.Text := 'Sample button';
+    lKB.Keyboard := [[lBtn, lBtn], [lBtn, lBtn, lBtn]];
+    lMsg.ChatId := AMsg.Chat.ID;
+    lMsg.Text := 'Choose';
+    lMsg.ReplyMarkup := lKB;
+    fBot.SendMessage(lMsg);
+  finally
+    lMsg.Free;
+    // lKB.Free;     <-- Autofree in TelegaPi core
+  end;
+end;
+
+procedure TDemoPooling.SendTextMessage(const UserLink: TtgUserLink; const MsgText: string);
+var
+  lMsg: TtgMessageArgument;
+begin
+  lMsg := TtgMessageArgument.Create;
+  try
+    lMsg.ChatId := UserLink;
+    lMsg.Text := MsgText;
+    fBot.SendMessage(lMsg);
+  finally
+    lMsg.Free;
+  end;
+end;
+
+procedure TDemoPooling.UpdateConsoleTitle(ABot: TTelegramBotApi);
+var
+  lUser: TtgUser;
+begin
+  lUser := ABot.GetMe.Result;
+  try
+    SetConsoleTitle(pwideChar(lUser.Username));
+  finally
+    // lUser.Free; <-- Autofree in TelegaPi core
+  end;
+end;
+
+procedure Main;
+var
+  lCore: TDemoPooling;
+begin
+  lCore := TDemoPooling.Create;
+  try
+    lCore.Main;
+  finally
+    lCore.Free;
+  end;
+end;
+
+begin
+  try
+    { TODO -oUser -cConsole Main : Insert code here }
+    Main;
+    Readln;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+
+end.
---
+++ TelegramBotApi.Router.pas
@@ -0,0 +1,107 @@
+﻿unit TelegramBotApi.Router;
+
+interface
+
+uses
+  System.Generics.Collections,
+  System.SysUtils;
+
+type
+  // TtgRouteUserState = TPair<Int64, string>;
+
+  TtgRouteUserStateManagerAbstract = class abstract
+  private
+    FOnGetUserStateCallback: TFunc<Int64, string>;
+    FOnSetUserStateCallback: TProc<Int64, string>;
+    FDefaultName: string;
+  protected
+    function DoGetUserState(const AUserID: Int64): string; virtual;
+    procedure DoSetUserState(const AIndex: Int64; const Value: string); virtual;
+  public
+    constructor Create; virtual;
+    property DefaultName: string read FDefaultName write FDefaultName;
+    property OnGetUserStateCallback: TFunc<Int64, string> read FOnGetUserStateCallback write FOnGetUserStateCallback;
+    property OnSetUserStateCallback: TProc<Int64, string> read FOnSetUserStateCallback write FOnSetUserStateCallback;
+    property UserState[const AIndex: Int64]: string read DoGetUserState write DoSetUserState;
+  end;
+
+  TtgRouteUserStateManagerRAM = class(TtgRouteUserStateManagerAbstract)
+  private
+    FRouteUserStates: TDictionary<Int64, string>;
+  protected
+    function DoGetUserState(const AUserID: Int64): string; override;
+    procedure DoSetUserState(const AIndex: Int64; const Value: string); override;
+  public
+    constructor Create;
+    destructor Destroy; override;
+  end;
+
+  TtgRoute = class
+  private
+    FName: string;
+  public
+    constructor Create(const AName: string);
+    property Name: string read FName write FName;
+
+  end;
+
+implementation
+
+{ TtgRouteUserStateManagerAbstract }
+
+constructor TtgRouteUserStateManagerAbstract.Create;
+begin
+  FDefaultName := '/start';
+end;
+
+function TtgRouteUserStateManagerAbstract.DoGetUserState(const AUserID: Int64): string;
+begin
+  if Assigned(FOnGetUserStateCallback) then
+    Result := FOnGetUserStateCallback(AUserID);
+  // if Result.IsEmpty then
+  // Result := FDefaultName;
+end;
+
+procedure TtgRouteUserStateManagerAbstract.DoSetUserState(const AIndex: Int64; const Value: string);
+begin
+  if Assigned(OnSetUserStateCallback) then
+    OnSetUserStateCallback(AIndex, Value);
+end;
+
+{ TtgRouteUserStateManagerRAM }
+
+constructor TtgRouteUserStateManagerRAM.Create;
+begin
+  inherited Create();
+  FRouteUserStates := TDictionary<Int64, string>.Create;
+end;
+
+destructor TtgRouteUserStateManagerRAM.Destroy;
+begin
+  FRouteUserStates.Free;
+  inherited Destroy;
+end;
+
+function TtgRouteUserStateManagerRAM.DoGetUserState(const AUserID: Int64): string;
+begin
+  // inherited DoGetUserState(AUserID);
+  if not FRouteUserStates.TryGetValue(AUserID, Result) then
+    Result := FDefaultName;
+
+end;
+
+procedure TtgRouteUserStateManagerRAM.DoSetUserState(const AIndex: Int64; const Value: string);
+begin
+  inherited DoSetUserState(AIndex, Value);
+  FRouteUserStates.AddOrSetValue(AIndex, Value);
+end;
+
+{ TtgRoute }
+
+constructor TtgRoute.Create(const AName: string);
+begin
+  inherited Create;
+  FName := AName;
+end;
+
+end.
